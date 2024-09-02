fd087818f4dae44877ff5503e7b09c9087210db8
Aruzhan <sazanovaa2@gmail.com>
2024-09-02 18:22:00 +0500

    comments

--- assignment1/my_blog/blog/models.py
+++ assignment1/my_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,3 +14,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
--- assignment1/my_blog/blog/views.py
+++ assignment1/my_blog/blog/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .models import Post
 from .serializers import PostSerializer
 
@@ -52,11 +53,31 @@ def post_detail(request, id):
         return Response({'message': 'City deleted successfully'}, status=204)
 def post_list_view(request):
     posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)  # Show 10 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
 
 def post_detail_view(request, id):
     post = get_object_or_404(Post, id=id)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form
+    })
 
 
 @login_required
