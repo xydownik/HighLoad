7cb30dd762dba0c432f98ad12ded697c0c10e1e4
Aruzhan <sazanovaa2@gmail.com>
2024-09-04 10:51:25 +0500

    last commit

--- assignment1/my_blog/blog/admin.py
+++ assignment1/my_blog/blog/admin.py
@@ -7,7 +7,7 @@ from .models import Post, Comment
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')  # Customize the list view
+    list_display = ('title', 'author', 'created_at')
     search_fields = ('title',)
 
 
